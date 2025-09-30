ccf97aab6f6976807960b44df5b7a0b297b1b48a
Quinn Flavel <quinnydf@yahoo.ca>
2025-09-30 05:42:21 +0000

    Suppress warning from `cpmpy`

--- src/jane_street_puzzles/__init__.py
+++ src/jane_street_puzzles/__init__.py
@@ -1,4 +1,8 @@
-from jane_street_puzzles.puzzle_2025_09 import main as puzzle_2025_09_main
+# TODO: waiting on <https://github.com/CPMpy/cpmpy/issues/709>
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, message=".*pkg_resources.*")
+
+from jane_street_puzzles.puzzle_2025_09 import main as puzzle_2025_09_main  # noqa: E402
 
 def main() -> None:
     puzzle_2025_09_main()
